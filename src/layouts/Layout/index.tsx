20568df0b86eebd12f19153f0ec55c8f68b7a3c9
yongu09 <klfm2415@naver.com>
2025-03-11 11:56:43 +0900

    Co-authored-by: yongu09 <klfm2415@naver.com>

--- src/layouts/Layout/index.tsx
+++ src/layouts/Layout/index.tsx
@@ -4,9 +4,28 @@ import { Outlet } from 'react-router'
 import './style.css';
 
 export default function Layout() {
+
+  // render: 공통 레이아웃 컴포넌트 렌더링 //
   return (
-    <div>
-      <Outlet />
+    <div id='layout-wrapper'>
+      <div id='top-bar'>
+        <div className='navigation'>
+          <div className='title'>Memories</div>
+          <div className='navigation-list'>
+            <div className='navigation-item active'>기억력 검사</div>
+            <div className='navigation-item'>집중력 검사</div>
+          </div>
+        </div>
+        <div className='my-content'>
+          <div className='my-content-list'>
+            <div className='my-content-item'>일기</div>
+            <div className='my-content-item'>로그아웃</div>
+          </div>
+        </div>
+      </div>
+      <div id='main'>
+        <Outlet />
+      </div>
     </div>
   )
 }
