e5949d199c34df191b83ec2822b9aa0db3e8db10
yongu09 <klfm2415@naver.com>
2025-03-17 10:54:51 +0900

    2025.03.17. 10:50

--- src/views/Auth/SignIn/index.tsx
+++ src/views/Auth/SignIn/index.tsx
@@ -1,7 +1,13 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
 import './style.css';
 import InputBox from 'src/components/InputBox';
 import { AuthPage } from 'src/types/aliases';
+import { signInRequest } from 'src/apis';
+import { SignInRequestDto } from 'src/apis/dto/request/auth';
+import { SignInResponseDto } from 'src/apis/dto/response/auth';
+import { ResponseDto } from 'src/apis/dto/response';
+import { useNavigate } from 'react-router';
 
 
 // interface: 로그인 컴포넌트 속성 //
@@ -14,6 +20,9 @@ export default function SignIn(props: Props) {
 
   const { onPageChange } = props;
 
+  // state: cookie 상태 //
+  const [_, setCookie] = useCookies();
+
   // state: 유저 아이디 상태 //
   const [userId, setUserId] = useState<string>('');
   // state: 유저 비밀번호 상태 //
@@ -22,6 +31,29 @@ export default function SignIn(props: Props) {
   const [userIdMessage, setUserIdMessage] = useState<string>('');
   // state: 유저 비밀번호 메세지 상태 //
   const [userPasswordMessage, setUserPasswordMessage] = useState<string>('');
+
+  // function: 네비게이터 함수 //
+  const navigator = useNavigate()
+
+  // function: sign in response 처리 함수 //
+  const signInResponse = (responseBody: SignInResponseDto | ResponseDto | null) => {
+    const message = 
+      !responseBody ? '서버에 문제가 있습니다' :
+      responseBody.code === 'DBE' ? '서버에 문제가 있습니다' :
+      responseBody.code === 'SF' ? '로그인 정보가 일치하지 않습니다' : '';
+
+    const isSuccess = responseBody !== null && responseBody.code === 'SU';
+    if (!isSuccess) {
+      setUserPasswordMessage(message);
+      return;
+    }
+
+    const { accessToken, expiration } = responseBody as SignInResponseDto;
+    const expires = new Date(Date.now() + expiration * 1000)
+    setCookie('accessToken', accessToken, { path: '/', expires });
+
+    navigator('/');
+  };
 
   // event handler: 유저 아이디 변경 이벤트 처리 //
   const onUserIdChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
@@ -41,7 +73,10 @@ export default function SignIn(props: Props) {
     if (!userPassword) setUserPasswordMessage('비밀번호를 입력하세요.');
     if (!userId || !userPassword) return;
 
-    // todo: 로그인 처리 로직 //
+    const requestBody: SignInRequestDto = {
+      userId, userPassword
+    }
+    signInRequest(requestBody).then(signInResponse);
   };
 
   // effect: 아이디 혹은 비밀번호 변경시 실행할 함수 //
