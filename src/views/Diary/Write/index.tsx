b9a734233a0f99a1a89a603d0b4329a6b3f445c2
yongu09 <klfm2415@naver.com>
2025-03-20 09:55:37 +0900

    2025.03.20. 09:50

--- src/views/Diary/Write/index.tsx
+++ src/views/Diary/Write/index.tsx
@@ -1,12 +1,58 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import './style.css';
 import TextEditor from 'src/components/TextEditor';
+import { Feeling, Weather } from 'src/types/aliases';
 
 // component: 일기 작성 화면 컴포넌트 //
 export default function DiaryWrite() {
 
+  // state: 일기 작성 내용 상태 //
+  const [weather, setWeather] = useState<Weather | ''>('');
+  const [felling, setFeeling] = useState<Feeling | ''>('');
+  const [title, setTitle] = useState<string>('');
   const [content, setContent] = useState<string>('');
 
+  // variable: 날씨 컨텐츠 클래스 //
+  const sunContentClass = 
+    weather === '맑음' ? 'content active' : 'content pointer';
+  const cloudContentClass = 
+    weather === '흐림' ? 'content active' : 'content pointer';
+  const rainContentClass = 
+    weather === '비' ? 'content active' : 'content pointer';
+  const snowContentClass = 
+    weather === '눈' ? 'content active' : 'content pointer';
+  const fogContentClass = 
+    weather === '안개' ? 'content active' : 'content pointer';
+
+  // variable: 날씨 아이콘 클래스 //
+  const sunIconClass = 
+    weather === '맑음' ? 'weathe-icon sun active' : 'weather-icon sun';
+  const cloudIconClass = 
+    weather === '흐림' ? 'weathe-icon cloud active' : 'weather-icon cloud';
+  const rainIconClass = 
+    weather === '비' ? 'weathe-icon rain active' : 'weather-icon rain';
+  const snowIconClass = 
+    weather === '눈' ? 'weathe-icon snow active' : 'weather-icon snow';
+  const fogIconClass = 
+    weather === '안개' ? 'weathe-icon fog active' : 'weather-icon fog';
+
+  // event handler: 날씨 변경 이벤트 처리 //
+  const onWeatherChangeHandler = (weather: Weather) => {
+    setWeather(weather);
+  };
+
+  // event handler: 기분 변경 이벤트 처리 //
+  const onFeelingChangeHandler = (feeling: Feeling) => {
+    setFeeling(feeling);
+  };
+
+  // event handler: 제목 변경 이벤트 처리 //
+  const onTitleChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setTitle(value);
+  };
+
+  // event handler: 내용 변경 이벤트 처리 //
   const onContentChangeHandler = (content: string) => {
     setContent(content);
   };
@@ -22,48 +68,47 @@ export default function DiaryWrite() {
             <div className='content'>2025-03-19</div>
           </div>
           <div className='input-row-box'>
-            <div className='title'>날씨</div>
-            <div className='content'>
-              <div className='weather-icon sun'></div>맑음
+          <div className='title'>날씨</div>
+            <div className={sunContentClass} onClick={() => onWeatherChangeHandler('맑음')}>
+              <div className={sunIconClass}></div>맑음
             </div>
-            <div className='content'>
-              <div className='weather-icon cloud'></div>흐림
+            <div className={cloudContentClass} onClick={() => onWeatherChangeHandler('흐림')}>
+              <div className={cloudIconClass}></div>흐림
             </div>
-            <div className='content'>
-              <div className='weather-icon rain'></div>비
+            <div className={rainContentClass} onClick={() => onWeatherChangeHandler('비')}>
+              <div className={rainIconClass}></div>비
             </div>
-            <div className='content'>
-              <div className='weather-icon snow'></div>눈
+            <div className={snowContentClass} onClick={() => onWeatherChangeHandler('눈')}>
+              <div className={snowIconClass}></div>눈
             </div>
-            <div className='content'>
-              <div className='weather-icon fog'></div>안개
+            <div className={fogContentClass} onClick={() => onWeatherChangeHandler('안개')}>
+              <div className={fogIconClass}></div>안개
             </div>
           </div>
           <div className='input-row-box'>
             <div className='title'>기분</div>
             <div className='content'>
-              <div className='feeling-icon happy'></div>행복
+              <div className='feeling-icon happy' onClick={() => onFeelingChangeHandler('행복')}></div>행복
             </div>
             <div className='content'>
-              <div className='feeling-icon funny'></div>즐거움
+              <div className='feeling-icon funny' onClick={() => onFeelingChangeHandler('즐거움')}></div>즐거움
             </div>
             <div className='content'>
-              <div className='feeling-icon normal'></div>보통
+              <div className='feeling-icon normal' onClick={() => onFeelingChangeHandler('보통')}></div>보통
             </div>
             <div className='content'>
-              <div className='feeling-icon sad'></div>슬픔
+              <div className='feeling-icon sad' onClick={() => onFeelingChangeHandler('슬픔')}></div>슬픔
             </div>
             <div className='content'>
-              <div className='feeling-icon angry'></div>분노
+              <div className='feeling-icon angry' onClick={() => onFeelingChangeHandler('분노')}></div>분노
             </div>
           </div>
           <div className='input-column-box'>
             <div className='title'>제목</div>
-            <input placeholder='제목을 입력하세요' />
+            <input type='text' value={title} placeholder='제목을 입력하세요' onChange={onTitleChangeHandler} />
           </div>
           <div className='input-column-box'>
-            <div className='title'>내용</div>
-            <TextEditor content={content} setContent={onContentChangeHandler} />
+            <div className='button middle disable'>작성 완료</div>
           </div>
         </div>
       </div>
