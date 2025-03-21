2cedc3602040b21ee6dab9f4a4416d5bcf68fee1
yongu09 <klfm2415@naver.com>
2025-03-21 10:50:57 +0900

    2025.03.21 10:50

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import DiaryMain from './views/Diary';
 import DiaryWrite from './views/Diary/Write';
 import DiaryDetail from './views/Diary/Detail';
 import DiaryUpdate from './views/Diary/Update';
+import MemoryTest from './views/MemoryTest';
 
 // Router 구성
 // - /auth : 로그인 및 회원가입 페이지
@@ -38,7 +39,7 @@ function App() {
         <Route path={MAIN_PATH} element={<>메인 페이지</>} />
 
         <Route path={MEMORY_TEST_PATH}>
-          <Route index element={<>기억력 검사 페이지</>} />
+          <Route index element={<MemoryTest />} />
           <Route path={MEMORY_TEST_COMPLETE_PATH} element={<>기억력 검사 완료 페이지</>} />
         </Route>
 
---
+++ src/views/MemoryTest/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css';
+
+// component: 기억력 검사 화면 컴포넌트 //
+export default function MemoryTest() {
+
+  // render: 기억력 검사 화면 컴포넌트 렌더링 //
+  return (
+    <div id='memory-test-wrapper'>
+      <div className='container'></div>
+    </div>
+  )
+}
